46fef87e55af08f070c4113cf43746ac9c0c90b2
roberto <roberto.scinocca@gmail.com>
2015-11-28 19:42:39 +0100

    -Spostati i contenuti delle pagine su file json -Prime implementazioni della pagina admin

---
+++ LucaBattistaWebsite/admin/bio.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Bio</a>
+            </div>
+            <!-- Top Menu Items -->
+            <ul class="nav navbar-right top-nav">
+                
+            </ul>
+            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+            <?php include './parts/sidebar.php'; ?>
+            <!-- /.navbar-collapse -->
+        </nav>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Bio
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-file"></i> Bio
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            
+            <div id="page-wrapper">
+                
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h2>Titolo</h2>
+                        <div class="input-group" id="bio_title">
+                            <input type="text" class="form-control" placeholder="Titolo" aria-describedby="basic-addon1">
+                          </div>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h2>Descrizione</h2>
+                        <div class="input-group" id="bio_description">
+                            <input type="text" class="form-control" placeholder="Descrizione" aria-describedby="basic-addon1">
+                          </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <button type="button" style="margin-top: 50px" class="btn btn-primary btn-lg">
+                            <span class="glyphicon glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> 
+                            Salva
+                          </button>
+                    </div>
+                </div>
+                
+            </div>
+            <!-- /.container-fluid -->
+
+            <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"> </script>
+
+                <script>
+                $(function() {
+                   $.getJSON('../../../../content/bio.json', function(data) {
+
+                       $("#bio_title input").val(data.title);
+                       $("#bio_description input").val(data.description);
+
+                   });
+                });
+                </script>
+            
+        </div>
+        <!-- /#page-wrapper -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ LucaBattistaWebsite/admin/parts/sidebar.php
@@ -0,0 +1,22 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+        </li>
+        <li>
+            <a href="home.php"><i class="fa fa-fw fa-dot-circle-o"></i> Home</a>
+        </li>
+        <li>
+            <a href="bio.php"><i class="fa fa-fw fa-dot-circle-o"></i> Bio</a>
+        </li>
+        <li>
+            <a href="music.php"><i class="fa fa-fw fa-dot-circle-o"></i> Music</a>
+        </li>
+        <li>
+            <a href="shop.php"><i class="fa fa-fw fa-dot-circle-o"></i> Shop</a>
+        </li>
+        <li>
+            <a href="contact.php"><i class="fa fa-fw fa-dot-circle-o"></i> Contacts</a>
+        </li>
+        <!-- class="active" -->
+</div>
